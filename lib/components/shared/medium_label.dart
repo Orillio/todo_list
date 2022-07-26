6b14cd54a772839d9db64fa6e88b0fc69ec595bc
Ян Козыренко <kopn48@mail.ru>
2022-07-26 13:54:19 +0300

    add importance and date showing

--- lib/components/shared/medium_label.dart
+++ lib/components/shared/medium_label.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 
-class MediumLabel extends StatelessWidget {
+class SmallLabel extends StatelessWidget {
   final String text;
-  const MediumLabel(this.text, {Key? key}) : super(key: key);
+  const SmallLabel(this.text, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: Theme.of(context).textTheme.labelMedium,
-      maxLines: 3,
-      overflow: TextOverflow.ellipsis,
+      style: Theme.of(context).textTheme.labelSmall,
     );
   }
 }
