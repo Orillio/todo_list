cff81613e25c0af0e4ba897d02271d91f6004047
Ян Козыренко <kopn48@mail.ru>
2022-08-25 22:10:51 +0300

    remove env

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:logger/logger.dart';
@@ -52,7 +51,6 @@ Future noZonedGuardedMain({required FlavorConfig config}) async {
   TodoAppServices.registerFirebaseAnalytics();
   TodoAppServices.registerFlavorConfig(config);
 
-  await dotenv.load(fileName: ".env");
 
   runApp(const MyApp());
 }
